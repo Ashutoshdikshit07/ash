396568506b364c023aac90f0d209d36c5950502a
Ashutosh Dikshit <ashutoshdikshit16@gmail.com>
2024-10-22 21:52:00 -0400

    signin page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-
 import './App.css'
+import {Routes, BrowserRouter, Route} from 'react-router-dom'
+import { Index } from './pages/Index'
+import { Signin } from './pages/Signin'
+
+
 
 function App() {
 
   return (
     <>
-          asdasd
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Index/>} />
+          <Route path='/signin' element={<Signin/>} />
+        </Routes>
+      </BrowserRouter>
+
           
     </>
   )
