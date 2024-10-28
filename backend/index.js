fe35c4f4f37508cf80b4e701e6451fb7c57c4ab3
Ashutosh Dikshit <ashutoshdikshit16@gmail.com>
2024-10-28 18:53:01 -0400

    updating vercel.json 3 and server.js file created to run locally

--- backend/index.js
+++ backend/index.js
@@ -15,5 +15,7 @@ app.get("/", (req, res)=>{
 
 })
     
-app.listen(3000)
+// app.listen(3000)
+
+module.exports = app;
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,12 @@
 const express = require("express")
 
 const userRouter = require("./user")
-
-
 const router = express.Router()
 
 
 router.use("/user",userRouter)
+
+
 
 
 router.get("/index1", (req, res)=>{
